48ac383e3d2f1c81453c297517e8aafc396e5021
Mishayaa <misha.chayka.2004@gmail.com>
2023-09-13 19:53:07 +0300

    init

---
+++ src/main/java/com/example/chaika_delivery/ChaikaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaika_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaikaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaikaDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chaika_delivery/ChaikaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaika_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaikaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
